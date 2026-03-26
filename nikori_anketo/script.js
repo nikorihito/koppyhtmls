12e62d00468c8cc377b6d529ce16545ac22e0502
nikorihito <140384316+nikorihito@users.noreply.github.com>
2026-03-26 22:20:57 +0900

    Delete nikori_anketo directory

--- nikori_anketo/script.js
+++
@@ -1,7 +0,0 @@
-function addQuestion() {
-  const questionList = document.getElementById("questionList");
-  const input = document.createElement("input");
-  input.type = "text";
-  input.placeholder = "質問を入力";
-  questionList.appendChild(input);
-}
